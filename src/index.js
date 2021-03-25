042352da2652698c69500ab7979b11c5605f5bb5
Максим Бельфер <maxbelf1@gmail.com>
2021-03-25 09:18:57 +0300

    Добавлен react к приложению

--- src/index.js
+++ src/index.js
@@ -1,46 +1,35 @@
-import {createStore} from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import { createStore, bindActionCreators } from 'redux'
+import reducer from './reducer'
+// import * as actions from './actions'
+import App from './components/app'
 
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-    case 'DEC':
-      return state - 1;
-    case 'RND':
-      return state + action.value;
-    default:
-      return state;
-
-  }
-}
-
-const inc = ()=> ({type: 'INC'})
-const dec = ()=> ({type: 'DEC'})
-const rnd = (value)=> ({type: 'RND', value})
+// Функция bindActionCreators from REDUX
+// const bindActionCreator = (creator, dispatch) => (...args)=> {
+//   dispatch(creator(...args))
+// }
 
 const store = createStore(reducer)
-
-document.querySelector('#inc').addEventListener('click', () => {
-  store.dispatch(inc())
-})
-document.querySelector('#dec').addEventListener('click', () => {
-  store.dispatch(dec())
-})
-document.querySelector('#rnd').addEventListener('click', () => {
-  const randomValue = Math.floor(Math.random() * 10)
-  store.dispatch(rnd(randomValue))
-})
-
-const update = () => {
-  document.getElementById('counter').textContent = store.getState()
-}
-
-store.subscribe(update)
+// const { dispatch } = store
 
 
-// let state = reducer(undefined, {})
-//  state = reducer(state, {type: 'INC'})
-// console.log(state)
-// state = reducer(state, { type: 'INC' })
-// console.log(state)
+// const {inc,dec, rnd, res} = bindActionCreators(actions, dispatch)
+
+// document.querySelector('#inc').addEventListener('click', inc)
+// document.querySelector('#dec').addEventListener('click', dec)
+// document.querySelector('#res').addEventListener('click', res)
+// document.querySelector('#rnd').addEventListener('click', () => {
+//   const randomValue = Math.floor(Math.random() * 10)
+//   rnd(randomValue)
+// })
+
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, document.getElementById('root'))
+
+
 
