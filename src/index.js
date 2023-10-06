c4eba9e6ba12b11a90e43d731ae42385fda084e4
Belfer Maksim <maxbelf1@gmail.com>
2023-10-06 15:09:01 +0300

    fixed old useless import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import { createStore, bindActionCreators } from 'redux'
+import { createStore } from 'redux'
 import reducer from './reducer'
 // import * as actions from './actions'
 import App from './components/app'
